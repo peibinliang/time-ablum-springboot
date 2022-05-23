4a6f7c3fcd65098f573cc448d9a74a6fd91cb4c8
peibinliang <1805993643@qq.com>
2022-05-23 16:50:45 +0800

    项目初始化

---
+++ src/test/java/com/time/album/TimeAlbumSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time.album;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeAlbumSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
